318bdee39be8ccaa60aa212e00081fc076fd6de2
Jannis <schaefer.jannis@googlemail.com>
2014-04-14 15:50:19 +0200

    A

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@ if [ ! -f tools/FAKE/tools/Fake.exe ]; then
   mono --runtime=v4.0 src/.nuget/NuGet.exe install FSharp.Formatting.CommandTool -OutputDirectory ./tools -ExcludeVersion -Prerelease 
   mono --runtime=v4.0 src/.nuget/NuGet.exe install SourceLink.Fake -OutputDirectory ./tools -ExcludeVersion 
 fi
-mono --runtime=v4.0 tools/FAKE/tools/FAKE.exe build.fsx $@
+ls
+ls tools
+ls ./tools
+ls src
+ls src/tools
+mono --runtime=v4.0 ./tools/FAKE/tools/FAKE.exe build.fsx $@
